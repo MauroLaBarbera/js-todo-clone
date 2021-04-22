34f243424688f7526007199c22bc1f805a2b4874
MauroLaBarbera <you@example.com>
2021-04-22 20:57:45 +0200

    add, remove, sbarrato

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const app = new Vue({
             {
                 text: 'Fare i compiti',
                 completed: false,
-            },    
+            },
             {
                 text: 'Fare la spesa',
                 completed: false,
@@ -17,7 +17,38 @@ const app = new Vue({
             {
                 text: 'Fare la lavatrice',
                 completed: false,
-            }, 
+            },
         ],
+        newTodo: '',
+    },
+    methods: {
+        //INSERIMENTO NUOVO TODO
+        addTodo() {
+
+            if(this.newTodo !== '') {
+
+                this.todos.push({
+                    text: this.newTodo,
+                    completed: false,
+                });
+
+                //RESET
+                this.newTodo = '';
+                this.$refs.todoInput.focus();
+            }
+        },
+
+        //RIMUOVIAMO TODO DALLA LISTA TRAMITE L?INDEX
+        removeTodo(index) {
+            this.todos.splice(index,1);
+
+            //POSSIAMO SALVARE L?ELEMENTO CANCELLATO DENTRO AD UNA CONST
+            // const deleted= this.todo.splice(index,1)[0];
+            // console.log(deleted);
+        },
+        updateStatus(index) {
+            this.todos[index].completed = ! this.todos[index].completed;
+            console.log(updateStatus(index));
+        },
     }
 });
