49685fff78ab3ed526d04e52f87b56ece0d3fc9c
MauroLaBarbera <you@example.com>
2021-04-22 20:09:23 +0200

    Layout todo, add array

--- js/main.js
+++ js/main.js
@@ -1,79 +1,23 @@
 /**
- * TODO APP
+ * To Do Vue-List!
  */
 
-$(document).ready(function() {
-
-//ARRAY CON OGGETTI GIA' INSERITI
-var todoItems = [
-    {
-        text: 'andare al super',
-        completed: false,
-    },
-    {
-        text: 'andare al super',
-        completed: true,
-    },
-    {
-        text: 'andare al super',
-        completed: false,
-    },
-    {
-        text: 'andare al super',
-        completed: true,
-    },
-    {
-        text: 'andare al super',
-        completed: false,
-    },
-];
-
-//REFERENZE 
-var list = $('.toDos');
-var newToDo = $('.add-ToDo');
-var template = $('#template li')
-
-    //RIEMPIAMO LA NOSTRA LISTA TODO
-    for (var i = 0; i < todoItems.length; i++ ) {
-        var toDo = todoItems[i];
-        //console.log(todoItems[i]);
-
-        //TEMPLATE
-        var item = template.clone();
-        item.find('.text').text(toDo.text);
-
-        //AGGIUNGIAMO ALLA FINE DELLA LISTA
-        list.append(item);
-
-        
-    };
-    //INSERIMENTO NUOVO TODO
-    newToDo.keyup(function(e) {
-        //console.log(e.which);
-
-        if(e.which === 13){
-            //console.log('enter');
-            var text = newToDo.val().trim();
-
-            //VALIDAZIONE
-            if(text !== ''){
-                //console.log(text);
-
-                //TEMPLATE
-                var item = template.clone();
-                item.find('.text').text(text);
-
-                //AGGIUNGIAMO ALLA FINE DELLA LISTA
-                list.append(item);
-            }
-        }
-    });
-
-
-
-
-
-
-
-    //END DOC READ
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {
+                text: 'Fare i compiti',
+                completed: false,
+            },    
+            {
+                text: 'Fare la spesa',
+                completed: false,
+            },
+            {
+                text: 'Fare la lavatrice',
+                completed: false,
+            }, 
+        ],
+    }
 });
